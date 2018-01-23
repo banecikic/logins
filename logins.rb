786e14fb85c1ed77013d78cbaaa2cfa1cfe3662f
Branimir Cikic <branimir.cikic@gmail.com>
2018-01-23 21:46:21 +0100

    Add files via upload

--- logins.rb
+++ logins.rb
@@ -1,5 +1,5 @@
 def rand_time
-  Time.at(rand * Time.now.to_i).year
+  Time.at(rand * Time.now.to_i)
 end
 
 
@@ -12,10 +12,13 @@ logins = { # ugly on purpose
   3 => [rand_time, rand_time, rand_time, rand_time, rand_time, rand_time, rand_time, rand_time],
 }
 
+outputHash = {}
 logins.each do |key, year_array|
-    counts = Hash.new 0
-    year_array.each do |year|
-      counts[year] +=1
-    end
-    puts "#{persons[key][1]}=>#{counts}"
+     counts = Hash.new 0
+      year_array.each do |year|
+        year_nr = year.year
+        counts[year_nr] +=1
+      end
+    outputHash[persons[key][1]] = counts
 end
+puts outputHash
